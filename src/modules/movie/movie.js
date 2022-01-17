abb2cd8ef55781f66ad0eb9ec97919fed5e71b57
Ashish Patel <ashishsushilpatel@gmail.com>
2022-01-17 23:31:43 +0000

    feat(movies): add form for new movie

--- src/modules/movie/movie.js
+++ src/modules/movie/movie.js
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { fetchMovies, selectAllMovies } from '../../slices/movieSlice'
 import MovieCard from '../../components/movieCard/movieCard'
+import DatePicker from 'react-datepicker'
 
+import 'react-datepicker/dist/react-datepicker.css'
+import axios from 'axios'
 const MovieGrid = ({ items }) => {
   return (
     <>
@@ -23,6 +26,40 @@ const Movies = () => {
       dispatch(fetchMovies())
     }
   }, [dispatch, status])
+
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    date: '',
+    startDate: '',
+    endDate: '',
+    isRandom: false,
+  })
+
+  const { title, date, startDate, endDate, isRandom } = newMovie
+
+  const handleInputChange = (e, date = false) => {
+    if (!date) {
+      let { title } = e.target
+      const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+      setNewMovie({ ...newMovie, [title]: value })
+    } else {
+      setNewMovie({ ...newMovie, [e.type]: e.date })
+    }
+  }
+
+  const [errorForm, setErrorForm] = useState(null)
+  const handleSubmitCreate = (e) => {
+    e.preventDefault()
+
+    if (!title) {
+      setErrorForm('Title cannot be blank')
+    } else {
+      axios.post('https://importerapi.prod.ashish.me/movies', newMovie, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      })
+      setErrorForm(null)
+    }
+  }
   return (
     <>
       <div className="custom-container">
@@ -33,6 +70,74 @@ const Movies = () => {
           </div>
         </section>
         <div className="section has-background-light p-5">
+          <form className={'form mb-5'} onSubmit={handleSubmitCreate}>
+            <fieldset className={'fieldset'}>
+              <div className="columns">
+                <div className="column">
+                  <div className="field is-grouped has-addons">
+                    <div className="control is-expanded ">
+                      <input
+                        className="input is-fullwidth"
+                        type="text"
+                        title="title"
+                        placeholder="Enter title"
+                        value={title}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                    {!newMovie.isRandom && (
+                      <div className="mr-3">
+                        <DatePicker
+                          placeholderText="Enter date"
+                          className="input"
+                          selected={date}
+                          onChange={(date) => handleInputChange({ type: 'date', date }, true)}
+                        />
+                      </div>
+                    )}
+                    {newMovie.isRandom && (
+                      <div className="mr-3">
+                        <DatePicker
+                          placeholderText="Enter start date"
+                          className="input"
+                          selected={startDate}
+                          onChange={(date) => handleInputChange({ type: 'startDate', date }, true)}
+                        />
+                      </div>
+                    )}
+                    {newMovie.isRandom && (
+                      <div className="mr-3">
+                        <DatePicker
+                          placeholderText="Enter end date"
+                          className="input"
+                          selected={endDate}
+                          onChange={(date) => handleInputChange({ type: 'endDate', date }, true)}
+                        />
+                      </div>
+                    )}
+                    <div className="mr-3">
+                      <label className="checkbox">
+                        <input
+                          title="isRandom"
+                          type="checkbox"
+                          checked={isRandom}
+                          onChange={handleInputChange}
+                        />
+                        &ensp; Generate Random <br></br>date
+                      </label>
+                    </div>
+                    <div className="control">
+                      <button type="submit" className={'button'}>
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {errorForm ? <div className={'info'}>{errorForm}</div> : null}
+            </fieldset>
+          </form>
           <div className="columns is-multiline">
             <MovieGrid items={movies} />
           </div>
